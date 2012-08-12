5c887870bdc344e8b37abd442a7fe57b4501327f
Judioo <git@Judioo.com>
2012-08-12 03:04:39 +0100

    added quiz view

--- Memrise/MEMRQuizViewController.h
+++ Memrise/MEMRQuizViewController.h
@@ -9,5 +9,35 @@
 #import <UIKit/UIKit.h>
 
 @interface MEMRQuizViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIView *questionView;
+@property (strong, nonatomic) IBOutlet UILabel *questionLabel;
+
+
+@property (strong, nonatomic) IBOutlet UIView *answer1View;
+@property (strong, nonatomic) IBOutlet UILabel *answer1Label;
+
+@property (strong, nonatomic) IBOutlet UIView *answer2View;
+@property (strong, nonatomic) IBOutlet UILabel *answer2Label;
+
+@property (strong, nonatomic) IBOutlet UIView *answer3View;
+@property (strong, nonatomic) IBOutlet UILabel *answer3Label;
+
+@property (strong, nonatomic) IBOutlet UIView *answer4View;
+@property (strong, nonatomic) IBOutlet UILabel *answer4Label;
+
+@property (strong, nonatomic) IBOutlet UIView *answer5View;
+@property (strong, nonatomic) IBOutlet UILabel *answer5Label;
+
+@property (strong, nonatomic) IBOutlet UIView *answer6View;
+@property (strong, nonatomic) IBOutlet UILabel *answer6Label;
+
+
+@property (strong, nonatomic) IBOutlet UIView *homeView;
+@property (strong, nonatomic) IBOutlet UIView *CommunityView;
+@property (strong, nonatomic) IBOutlet UIView *topicsView;
+@property (strong, nonatomic) IBOutlet UIView *aboutView;
+@property (strong, nonatomic) IBOutlet UIView *profileView;
+
+
 
 @end
--- Memrise/MEMRQuizViewController.m
+++ Memrise/MEMRQuizViewController.m
@@ -7,12 +7,39 @@
 //
 
 #import "MEMRQuizViewController.h"
+#import "UIViewController+formatView.h"
+#import "UIViewController+animateView.h"
+
+
+#define kAnswerCornerRadius     10
+#define kQuestionCornerRadius   20
 
 @interface MEMRQuizViewController ()
 
 @end
 
 @implementation MEMRQuizViewController
+@synthesize questionView;
+@synthesize questionLabel;
+@synthesize answer1View;
+@synthesize answer1Label;
+@synthesize answer2View;
+@synthesize answer2Label;
+@synthesize answer3View;
+@synthesize answer3Label;
+@synthesize answer4View;
+@synthesize answer4Label;
+@synthesize answer5View;
+@synthesize answer5Label;
+@synthesize answer6View;
+@synthesize answer6Label;
+
+@synthesize homeView;
+@synthesize CommunityView;
+@synthesize topicsView;
+@synthesize aboutView;
+@synthesize profileView;
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,11 +53,69 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    [self formatMenuViews];
+    [self configureTouch];
+    [self configureHome];
 }
+
+- (void)pushView
+{
+    [self performSegueWithIdentifier:@"quizToChoiceSegue" sender:self];
+}
+
+- (void)configureHome
+{
+    UITapGestureRecognizer *homeTap        = [[UITapGestureRecognizer alloc]
+                                               initWithTarget:self action:@selector(pushView)];
+    
+    [self.homeView addGestureRecognizer:homeTap];   
+}
+     
+- (void)formatMenuViews
+{
+    // menu views
+    [self roundViewsBottomCorner:self.homeView withRadius:5.0f];
+    [self roundViewsBottomCorner:self.CommunityView withRadius:5.0f];
+    [self roundViewsBottomCorner:self.topicsView withRadius:5.0f];
+    [self roundViewsBottomCorner:self.aboutView withRadius:5.0f];
+    [self roundViewsBottomCorner:self.profileView withRadius:5.0f];
+    
+    // question and answer views
+    [self roundViewsCorner:self.questionView withRadius:kQuestionCornerRadius];
+    [self roundViewsCorner:self.answer1View withRadius:kAnswerCornerRadius];
+    [self roundViewsCorner:self.answer2View withRadius:kAnswerCornerRadius];
+    [self roundViewsCorner:self.answer3View withRadius:kAnswerCornerRadius];
+    [self roundViewsCorner:self.answer4View withRadius:kAnswerCornerRadius];
+    [self roundViewsCorner:self.answer5View withRadius:kAnswerCornerRadius];
+    [self roundViewsCorner:self.answer6View withRadius:kAnswerCornerRadius];
+}
+
+- (void)configureTouch
+{
+    [self setMenuAsDenied:self.profileView];
+    [self setMenuAsDenied:self.CommunityView];
+    [self setMenuAsDenied:self.topicsView];
+    [self setMenuAsDenied:self.aboutView];
+}
+
 
 - (void)viewDidUnload
 {
+    [self setQuestionView:nil];
+    [self setQuestionLabel:nil];
+    [self setAnswer1View:nil];
+    [self setAnswer1Label:nil];
+    [self setAnswer2View:nil];
+    [self setAnswer2Label:nil];
+    [self setAnswer3View:nil];
+    [self setAnswer3Label:nil];
+    [self setAnswer4View:nil];
+    [self setAnswer4Label:nil];
+    [self setAnswer5View:nil];
+    [self setAnswer5Label:nil];
+    [self setAnswer6View:nil];
+    [self setAnswer6Label:nil];
+    [self setProfileView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
